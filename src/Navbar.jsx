152ecf6eb0cdfdd5737a21e40f0d063331cdf44c
Clement Maliki <clementugomaliki@gmail.com>
2026-07-18 10:39:40 +0100

    some responsiveness on navbar hambugger

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -16,7 +16,7 @@ export default function Navbar() {
       <header className="flex justify-between items-center px-10 sm:px-14 md:px-20 py-3 sm:py-4 md:py-6 bg-white fixed top-0 right-0 left-0 z-50">
       
          <Link to="/"><img src={smlogo} alt="logo" className="sm:hidden w-12"/></Link>
-          <Link to="/"><img src={logo} alt="Merch-by-Lucius logo" className="hidden md:w-15 sm:block sm:w-20 md:w-28" /></Link>
+          <Link to="/"><img src={logo} alt="Merch-by-Lucius logo" className="hidden sm:block sm:w-24 md:w-28" /></Link>
       
         <div className="flex space-x-8 text-base">
           <Link to="/materials" className=" hidden md:block font-semibold">Discover</Link>
@@ -28,19 +28,27 @@ export default function Navbar() {
           
           <Link className="hidden md:block bg-[#2EC5BC] text-white px-8 py-3 rounded-full cursor-pointer">Become an Affiliate</Link>
         </div>
+        <div className="relative z-40">
         <button 
-        className="md:hidden"
-        onClick={toggleMenu}>
-          {isMenuOpen ? <LuX/> : <LuMenu/>}
-          {isMenuOpen && (
-            <div className="flex flex-col items-start">
-            <Link>Discover</Link>
-            <Link>Blog</Link>
-            <Link>Login</Link>
-            <Link>Become an Affiliate</Link>
-            </div>
-          )}
+          className="md:hidden w-12 h-10" onClick={toggleMenu}
+           aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          >{isMenuOpen ? <LuX/> : <LuMenu/>}
+           
         </button>
+        {isMenuOpen && (
+          
+            <div className="fixed inset-0 bg-[#2EC5BC]/20 z-30" onClick={()=> setIsMenuOpen(false)}/>)}
+            <div className={`flex flex-col items-start text-left absolute top-full right-0 mt-2 text-sm p-4 font-semibold z-40 transition-all duration-700 origin-top-right divide-y divide-white/40 bg-[#2EC5BC] space-y-4 w-48 text-white ${
+              isMenuOpen ? "opacity-100 scale-100 pointer-events-auto" : "opacity-0 scale-95 pointer-events-none"
+            }`}>
+              <Link to="/materials" onClick={()=> setIsMenuOpen(false)}>Discover</Link>
+              <Link to="/posts" onClick={()=> setIsMenuOpen(false)}>Blog</Link>
+              <Link to="/sign-in" onClick={()=> setIsMenuOpen(false)}>Login</Link>
+              <Link onClick={()=> setIsMenuOpen(false)}>Become an Affiliate</Link>
+            </div>
+          
+          
+        </div>
       </header>
     </>
   )
