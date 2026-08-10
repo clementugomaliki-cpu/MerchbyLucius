39161dbf1bef69f869e5ef2ed3d72459edaf7ff8
Clement Maliki <clementugomaliki@gmail.com>
2026-08-10 11:49:46 +0100

    set modal for payment on account settings page

--- src/Settings.jsx
+++ src/Settings.jsx
@@ -1,4 +1,4 @@
-//import { useState } from "react";
+import { useState } from "react";
 import PagesHeader from "./Components/PagesHeader";
 import Sidebar from "./Components/Sidebar";
 import avatar from "./images/User Avatar.png";
@@ -7,6 +7,7 @@ import { FaRegUser } from "react-icons/fa";
 import { MdOutlineSecurity, MdOutlineLock, MdOutlineScreenLockPortrait, MdOutlineCreditCard, MdOutlineMoreVert, MdAddCircleOutline } from "react-icons/md";
 
 export default function Settings() {
+  const [modal, setModal] = useState(false);
     
   return (
     <div>
@@ -56,12 +57,12 @@ export default function Settings() {
         <div className="bg-white rounded-xl p-8 gap-6 mt-10">
           <div className="flex flex-col gap-2 text-[#002F71]">
             <div className="flex items-center gap-2 font-bold text-[#002F71] text-base">
-               <MdOutlineSecurity/>
+               <MdOutlineSecurity className="text-xl"/>
               <p>Account Security</p>
             </div>
             <div className="rounded-xl bg-[#F2F4F5] flex justify-between p-4">
                 <div className="flex items-center gap-3 font-bold text-base text-[#4A5568]">
-                <MdOutlineLock/>
+                <MdOutlineLock className="text-xl"/>
                   <div className="flex flex-col">  
                     <span className="">Password</span>
                     <span className="font-[400] text-[#191C1D]">{`Last changed ${3} months ago`}</span>
@@ -73,7 +74,7 @@ export default function Settings() {
 
              <div className="rounded-xl bg-[#F2F4F5] flex justify-between p-4 mt-5">
                 <div className="flex items-center gap-3 font-bold text-base text-[#4A5568]">
-                <MdOutlineScreenLockPortrait/>
+                <MdOutlineScreenLockPortrait className="text-xl"/>
                   <div className="flex flex-col">  
                     <span className="">2FA</span>
                     <span className="font-[400] text-[#191C1D]">{`Disabled`}</span>
@@ -88,7 +89,7 @@ export default function Settings() {
         <div className="bg-white rounded-xl p-8 gap-6 flex flex-col mt-3 lg:mt-10">
           <div className="flex flex-col gap-2 text-[#002F71]">
             <div className="flex items-center gap-2 font-bold text-[#002F71] text-base">
-               <MdOutlineCreditCard/>
+               <MdOutlineCreditCard className="text-xl"/>
               <p>Saved Billing</p>
             </div>
             <div className="rounded-xl bg-white border border-[#BBC9C7]/50 flex justify-between items-center p-4">
@@ -99,17 +100,50 @@ export default function Settings() {
                     <span className="font-[400] text-[#191C1D]">{`Expires ${"12/26"}`}</span>
                   </div>
                 </div>
-                <MdOutlineMoreVert className="cursor-pointer"/>
+                <MdOutlineMoreVert className="cursor-pointer text-xl"/>
               
             </div>
-
-             <div className="rounded-xl bg-white border-2 border-dashed border-[#BBC9C7]/50 flex justify-between p-4 mt-5">
-                <div className="flex items-center mx-auto gap-2 font-bold text-base text-[#4A5568]">
-                    <MdAddCircleOutline/>
+                <button onClick={()=>setModal(!modal)}
+                className="flex items-center gap-2 font-bold text-[#4A5568] cursor-pointer rounded-xl mt-5 border-3 border-[#BBC9C7]/50 justify-center bg-white p-4 w-full border-dashed">
+                    <MdAddCircleOutline className="text-xl"/>
                     <span className="">Add Payment Method</span>
-                </div>              
-            </div>
-
+                </button>  
+                {modal && (
+                  <div className="flex fixed inset-0 bg-black/50 z-50 items-center justify-center" onClick={()=>setModal(false)}>
+                    <div className="flex flex-col bg-white rounded-md px-6" onClick={(e)=>e.stopPropagation()}>
+                      <h4 className="font-bold text-2xl text-[#002F71] py-8">Add Payment Method</h4>
+                      <div className="flex flex-col gap-2">
+                          <span className="text-[10px] text-[#3C4948] font-[400]">CARDHOLDER NAME</span>
+                          <input type="text" className="px-4 pt-[13px] pb-[14px] bg-[#F2F4F5] rounded-md outline-none" />
+                      </div>
+                      <div className="flex flex-col gap-2 pt-6">
+                          <span className="text-[10px] text-[#3C4948] font-[400]">CARD NUMBER</span>                          
+                            <div className="px-4 pt-[13px] pb-[14px] bg-[#F2F4F5] rounded-md flex items-center gap-2">
+                              <MdOutlineCreditCard className="text-xl"/>
+                              <input placeholder="0000 0000 0000 0000" className="outline-none"/>
+                            </div>   
+                      </div>
+                      <div className="grid grid-cols-2 gap-6">
+                      <div className="flex flex-col gap-2 pt-6">
+                          <span className="text-[10px] text-[#3C4948] font-[400]">EXPIRY DATE</span>                          
+                            <input className="px-4 pt-[13px] pb-[14px] bg-[#F2F4F5] rounded-md flex items-center gap-2 outline-none" placeholder="MM/YY"/>
+                            
+                      </div>
+                      <div className="flex flex-col gap-2 pt-6">
+                          <span className="text-[10px] text-[#3C4948] font-[400]">CVV</span>                          
+                            <input className="px-4 pt-[13px] pb-[14px] bg-[#F2F4F5] rounded-md flex items-center gap-2" placeholder="123"/>  
+                      </div>
+                      </div>
+                      <div className="flex gap-2 text-[#3C4948] font-[400] pt-6 items-center">
+                        <input type="checkbox" className="w-5 h-5 rounded-[4px] border border-[#6C7A78]" />
+                        Set as default payment method
+                      </div>
+                      <button onClick={()=>setModal(!modal)} 
+                        className="text-white font-bold py-2 px-4 rounded-full bg-[#2EC5BC] mt-6 cursor-pointer">Add Card</button>
+                      <button onClick={()=>setModal(!modal)} className="my-6 font-bold text-[#3C4948] cursor-pointer">Cancel</button>
+                    </div>
+                  </div>
+                )}            
           </div>
 
         </div>
